e2dc877036d1b1045b75023986eb99f5cb74ef85
brunomerenyi <bruno.merenyi@gmail.com>
2026-06-12 14:57:24 +0200

    ipv4

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
-
+const ipv4 =
+  process.env.NEXT_ALLOWED_HOSTS?.split(",").find((host) =>
+    host.includes("."),
+  ) ?? "172.20.10.6";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   // Whitelists your local IP network so Turbopack serves assets and fonts without 403 blocks
-  allowedDevOrigins: ["10.95.223.238", "localhost:3000"],
+  allowedDevOrigins: [ipv4, "localhost:3000"],
 };
 
 export default nextConfig;
